0e7f55a3195ae143943e77817e5ed34a627f3005
GwenfromFinance <rhysa@outlook.com>
2024-10-30 12:23:39 +0000

    finished project

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-    console.log("hi")
-})
+document.addEventListener("DOMContentLoaded", function() {
+    let password = document.querySelector("#password");
+    let confirmPassword = document.querySelector("#confirmPassword");
+    let error = document.querySelector("#error");
+
+    function checkPasswordMatch() {
+        if (password.value === "" || confirmPassword.value === "") {
+            error.innerHTML = "";
+            password.classList.remove("input-error");
+            confirmPassword.classList.remove("input-error");
+        } else if (password.value === confirmPassword.value) {
+            error.innerHTML = "";
+            password.classList.remove("input-error");
+            confirmPassword.classList.remove("input-error");
+        } else {
+            error.innerHTML = "*Passwords do not match";
+            password.classList.add("input-error");
+            confirmPassword.classList.add("input-error");
+        }
+    }
+
+    password.addEventListener("input", checkPasswordMatch);
+    confirmPassword.addEventListener("input", checkPasswordMatch);
+});
